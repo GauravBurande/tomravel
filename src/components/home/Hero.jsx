ddcaf0812e273965521e27b657fe24dfc91d2a1d
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-25 01:56:10 +0530

    intial setup and home UI

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className='flex items-center justify-center'>
+        this is hero section
+      </div>
+    </div>
+  )
+}
+
+export default Hero
