64bb3018ac27a0e332ecc7f1e4ac538936329c13
LabhK <labh.k2003@gmail.com>
2023-02-26 01:19:49 +0530

    Update Translations.jsx

--- src/components/Translations.jsx
+++ src/components/Translations.jsx
@@ -74,77 +74,80 @@ const Translations = () => {
   }, []);
 
 
-  if(options === []){
-    return(
+  if(options !== []){
+    return (
       <>
-        <Loading/>
+        <div className='text-center min-h-[85vh]'>
+          <h1 className='lg:text-5xl text-4xl font-bold py-3 pb-10'>Translation</h1>
+          <span className=' font-bold py-3 pr-6 pb-10 text-lg '>Select Language :</span><select className='bg-transparent border border-solid rounded-lg focus:bg-black' onChange={(e) => setTo(e.target.value)}>
+            {options.map((opt) => (
+              <option key={opt.code} value={opt.code}>
+                {opt.name}
+              </option>
+            ))}
+          </select>  
+        <div className='pt-5 pb-7'>
+        <button className='border border-solid rounded-lg p-2 ' onClick={defaultTranslate} >Generate Greetings</button>
+        </div>
+  
+        
+        <table className="table-auto text-2xl " align='center'>
+          <thead>
+          </thead>
+          <tbody>
+            <tr >
+              <td className='pb-4 pr-6' >{base[0]}</td>
+              <td className='pb-4 pr-6' >{array[0]}</td>
+  
+            </tr>
+            <tr>
+              <td className='pb-4 pr-6' >{base[1]}</td>
+              <td className='pb-4 pr-6' >{array[1]}</td>
+  
+            </tr>
+            <tr>
+              <td className='pb-4 pr-6' >{base[2]}</td>
+              <td className='pb-4 pr-6' >{array[2]}</td>
+  
+            </tr>
+            <tr>
+              <td className='pb-4 pr-6' >{base[3]}</td>
+              <td className='pb-4 pr-6' >{array[3]}</td>
+            </tr>
+            <tr>
+              <td className='pb-4 pr-6' >{base[4]}</td>
+              <td className='pb-4 pr-6' >{array[4]}</td>
+            </tr>
+          </tbody>
+        </table>
+  
+        {/* Enter text to translate     
+        <div>
+          
+          <input className='bg-transparent pt-10 border border-solid rounded-lg'
+            onInput={(e) => setInput(e.target.value)}
+          ></input>
+        </div>
+              Translated text
+        <div>
+          <div className='bg-transparent pt-10 border border-solid rounded-lg w-48'>{output}</div>
+        </div>
+        <div>
+          <button className='border border-solid rounded-lg' onClick={e=>translate()}>Translate</button>
+        </div> */}
+        </div>
       </>
     )
   }
 
-  else{return (
+  else{
+  return(
     <>
-      <div className='text-center min-h-[85vh]'>
-        <h1 className='lg:text-5xl text-4xl font-bold py-3 pb-10'>Translation</h1>
-        <span className=' font-bold py-3 pr-6 pb-10 text-lg '>Select Language :</span><select className='bg-transparent border border-solid rounded-lg focus:bg-black' onChange={(e) => setTo(e.target.value)}>
-          {options.map((opt) => (
-            <option key={opt.code} value={opt.code}>
-              {opt.name}
-            </option>
-          ))}
-        </select>  
-      <div className='pt-5 pb-7'>
-      <button className='border border-solid rounded-lg p-2 ' onClick={defaultTranslate} >Generate Greetings</button>
-      </div>
-
-      
-      <table className="table-auto text-2xl " align='center'>
-        <thead>
-        </thead>
-        <tbody>
-          <tr >
-            <td className='pb-4 pr-6' >{base[0]}</td>
-            <td className='pb-4 pr-6' >{array[0]}</td>
-
-          </tr>
-          <tr>
-            <td className='pb-4 pr-6' >{base[1]}</td>
-            <td className='pb-4 pr-6' >{array[1]}</td>
-
-          </tr>
-          <tr>
-            <td className='pb-4 pr-6' >{base[2]}</td>
-            <td className='pb-4 pr-6' >{array[2]}</td>
-
-          </tr>
-          <tr>
-            <td className='pb-4 pr-6' >{base[3]}</td>
-            <td className='pb-4 pr-6' >{array[3]}</td>
-          </tr>
-          <tr>
-            <td className='pb-4 pr-6' >{base[4]}</td>
-            <td className='pb-4 pr-6' >{array[4]}</td>
-          </tr>
-        </tbody>
-      </table>
-
-      {/* Enter text to translate     
-      <div>
-        
-        <input className='bg-transparent pt-10 border border-solid rounded-lg'
-          onInput={(e) => setInput(e.target.value)}
-        ></input>
-      </div>
-            Translated text
-      <div>
-        <div className='bg-transparent pt-10 border border-solid rounded-lg w-48'>{output}</div>
-      </div>
-      <div>
-        <button className='border border-solid rounded-lg' onClick={e=>translate()}>Translate</button>
-      </div> */}
-      </div>
+      <Loading/>
     </>
-  )}
+  )
+  }
+  
 }
 
 export default Translations
