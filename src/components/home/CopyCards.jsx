a3fd86dac9513985bcb8476f31d7e6a4ff214171
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-25 12:55:16 +0530

    Homepage: added copy (#6)
    * intial setup and home UI
    
    * home component: hero and osme copy
    
    * fixing errors
    
    * fixing errors
    
    * removed lorem and added copy

--- src/components/home/CopyCards.jsx
+++ src/components/home/CopyCards.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 const CopyCards = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
+        <h1 className='text-5xl font-bold'>What's Tomravel?</h1>
+
       <div className='flex flex-wrap pb-10 text-center xl:px-24 items-center justify-center'>
+
         <div>
             <img src="backpack.webp" alt="backpack" />
         </div>
         <div className='font-semibold mx-auto w-[50vw] xl:px-10'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus sit illo ipsa fuga, dignissimos eveniet quod sunt qui deleniti, similique animi quis omnis reprehenderit temporibus maiores aliquid voluptatibus delectus at. Iure, dicta quisquam! Aut sit vero in minima culpa vitae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, architecto!
+            Whether you're a traveler seeking new experiences or simply curious about the world, we're here to help you discover new and exciting places.
+            <p>At our core, we believe that diversity is something to be celebrated, not feared!</p>
         </div>
       </div>
 
@@ -17,7 +21,7 @@ const CopyCards = () => {
             <img src="vehicle.webp" alt="vehicle" />
         </div>
         <div className='font-semibold mx-auto w-[50vw] xl:px-10'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus sit illo ipsa fuga, dignissimos eveniet quod sunt qui deleniti, similique animi quis omnis reprehenderit temporibus maiores aliquid voluptatibus delectus at. Iure, dicta quisquam! Aut sit vero in minima culpa vitae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, architecto!
+            Our platform is designed to provide you with a ride across the globe, where you can explore the diversity of cultures and traditions that make our world so fascinating. We offer a range of resources and tools to help you on your journey, from travel guides to language resources to virtual tours.
         </div>
       </div>
 
@@ -26,7 +30,8 @@ const CopyCards = () => {
             <img src="world.webp" alt="world" />
         </div>
         <div className='font-semibold mx-auto w-[50vw] xl:px-10'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus sit illo ipsa fuga, dignissimos eveniet quod sunt qui deleniti, similique animi quis omnis reprehenderit temporibus maiores aliquid voluptatibus delectus at. Iure, dicta quisquam! Aut sit vero in minima culpa vitae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, architecto!
+            <p>Our mission is to bring people together to learn, celebrate, and support the beauty and richness of different cultures. Whether you're a traveler seeking new experiences or simply curious about the world, we're here to help you discover new and exciting places.</p>
+            Thank you for visiting our website, and we hope you enjoy your ride across the globe!
         </div>
       </div>
     </div>
