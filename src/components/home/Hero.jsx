2074d33614bcfb74fb9be2ce95730d4b75793ef0
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-25 12:50:42 +0530

    removed lorem and added copy

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <img src="travelgirl.webp" width={200} alt="travelgirl" />
         </div>
 
-        <div className='text-center'>
+        <div className='text-center w-2/3'>
           <h1 className='text-7xl font-bold py-3'>Explore the cultures...</h1>
           <p className='lg:px-72 md:px-52'>Welcome to tomravel, get to know more about new places while travelling, new places you have not visited before, get to know about their cultire and more.</p>
         </div>
