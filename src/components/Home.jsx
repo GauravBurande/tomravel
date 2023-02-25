778c4f97b94f7ebaa71f59ca5b3a0a0b66056cf7
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-25 12:10:49 +0530

    Home page: hero component and some copy (#5)
    * intial setup and home UI
    
    * home component: hero and osme copy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import CopyCards from './home/CopyCards'
 import Hero from './home/Hero'
 import Svglogo from './home/Svglogo'
 
@@ -6,14 +8,18 @@ const Home = () => {
   return (
     <div>
         <div className='flex px-10 py-5 items-center justify-between'>
-            <Svglogo/>
-
+            <div className='w-40'>
+              <Svglogo/>
+            </div>
             <div>
                 <button className='bg-yellow-200 text-black p-4 rounded-full font-semibold hover:bg-gradient-to-bl from-sky-200 via-sky-100 to-cyan-200'>Playground</button>
             </div>
         </div>
         
+        <div className='px-10'>
         <Hero/>
+        <CopyCards/>
+        </div>
     </div>
   )
 }
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <div className='flex items-center justify-center'>
-        this is hero section
-      </div>
+      <div className='flex flex-wrap items-center py-10 justify-center min-h-[550px]'>
+        <div>
+          <img src="travelgirl.webp" width={200} alt="travelgirl" />
+        </div>
+
+        <div className='text-center'>
+          <h1 className='text-7xl font-bold py-3'>Explore the cultures...</h1>
+          <p className='lg:px-72 md:px-52'>Welcome to tomravel, get to know more about new places while travelling, new places you have not visited before, get to know about their cultire and more.</p>
+        </div>
+        </div>
     </div>
   )
 }
