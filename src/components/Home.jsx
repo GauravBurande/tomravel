15f57469aec088486399560b79c391528a17b1f6
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-25 12:21:26 +0530

    fixing errors

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CopyCards from './home/CopyCards'
+import Hero from './home/Hero'
+import Svglogo from './home/Svglogo'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='flex px-10 py-5 items-center justify-between'>
+            <div className='w-40'>
+              <Svglogo/>
+            </div>
+
+            <div>
+                <button className='bg-yellow-200 text-black p-4 rounded-full font-semibold hover:bg-gradient-to-bl from-sky-200 via-sky-100 to-cyan-200'>Playground</button>
+            </div>
+        </div>
+        
+        <div className='px-10'>
+        <Hero/>
+        <CopyCards/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
